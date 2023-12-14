f89969f5b8a70f66e37bad00ece402d57f1ec4ae
Paul Salame <psalame@2B4.42angouleme.fr>
2023-12-14 15:14:15 +0100

    fix double death on mandatory

--- philo/src/philosopher/print_state.c
+++ philo/src/philosopher/print_state.c
@@ -6,7 +6,7 @@
 /*   By: psalame <psalame@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 17:34:28 by psalame           #+#    #+#             */
-/*   Updated: 2023/12/07 15:25:59 by psalame          ###   ########.fr       */
+/*   Updated: 2023/12/14 15:13:48 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,23 @@
 void	print_state(t_philosoph *philosoph, long time)
 {
 	pthread_mutex_lock(&philosoph->simulation->mutex);
-	if (philosoph->last_meal_date + philosoph->simulation->die_time < time)
-		printf("%ld %d died\n", time, philosoph->id);
-	else if (philosoph->simulation->active)
+	if (philosoph->simulation->active)
 	{
-		if (philosoph->state == eating)
+		if (philosoph->last_meal_date + philosoph->simulation->die_time < time)
+			printf("%ld %d died\n", time, philosoph->id);
+		else if (philosoph->simulation->active)
 		{
-			printf("%ld %d has taken a fork\n", time, philosoph->id);
-			printf("%ld %d has taken a fork\n", time, philosoph->id);
-			printf("%ld %d is eating\n", time, philosoph->id);
+			if (philosoph->state == eating)
+			{
+				printf("%ld %d has taken a fork\n", time, philosoph->id);
+				printf("%ld %d has taken a fork\n", time, philosoph->id);
+				printf("%ld %d is eating\n", time, philosoph->id);
+			}
+			else if (philosoph->state == sleeping)
+				printf("%ld %d is sleeping\n", time, philosoph->id);
+			else
+				printf("%ld %d is thinking\n", time, philosoph->id);
 		}
-		else if (philosoph->state == sleeping)
-			printf("%ld %d is sleeping\n", time, philosoph->id);
-		else
-			printf("%ld %d is thinking\n", time, philosoph->id);
 	}
 	pthread_mutex_unlock(&philosoph->simulation->mutex);
 }
